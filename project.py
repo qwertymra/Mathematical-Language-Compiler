3d106977408148f80a8bc15422a2fbd91f916f8a
Mayank Reddy Ankilla <79315030+qwertymra@users.noreply.github.com>
2023-05-16 11:58:49 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,36 @@
+from myparser import MyParser
+from word2number import w2n
+
+if __name__ == "__main__":
+    parser = MyParser()
+    while True:
+        # getting input and parse while 'ctrl+d' pressed
+        try:
+            s = input('Input Exp >>>> ')
+            op=['+','-','/','*']
+            operator=''
+            for o in op:
+                if o in s:
+                    operator=o
+                    break
+            splitted = s.split(operator)
+            # print(splitted[0],splitted[1])
+            exp1=splitted[0].strip()
+            exp2=splitted[1].strip()
+            try:
+                if not exp1.isnumeric():
+                    exp1=w2n.word_to_num(exp1)
+                if not exp2.isnumeric():
+                    exp2=w2n.word_to_num(exp2)
+                # print(exp1,exp2)
+                s=str(exp1)+operator+str(exp2)
+                # print(s)
+            except Exception as e:
+                pass
+        except EOFError:
+            break
+        if not s:
+            continue
+        result = parser.parse(s)
+
+        print(f"Result Is -> {result}\n")
